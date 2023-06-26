74c2b71e6c7cc768ab49818ef82258b7b61fd200
Owais Ahmad <owaisahmad014@gmail.com>
2023-06-26 23:18:51 +0500

    added routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,9 +5,55 @@ import asyncHandler from "express-async-handler";
 // @access  Public
 
 const authUser = asyncHandler(async (req, res) => {
+
     res.status(200).json({ message: 'Auth User Owais' });
 });
 
+
+
+// @desc    Register a new user
+// route    POST/api/users/auth
+// @access  Public
+
+const registerUser = asyncHandler(async (req, res) => {
+
+    res.status(200).json({ message: 'Register user' });
+});
+
+
+
+// @desc    Logout user
+// route    POST/api/users/logout
+// @access  Public
+
+const logoutUser = asyncHandler(async (req, res) => {
+
+    res.status(200).json({ message: 'Logout User  ' });
+});
+
+
+// @desc    Get user profile
+// route    Get/api/users/profile
+// @access  private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+
+    res.status(200).json({ message: '  User Profile' });
+});
+
+// @desc    Update user profile
+// route    Put/api/users/profile
+// @access  private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+
+    res.status(200).json({ message: 'Update user profile  ' });
+});
+
 export {
-    authUser
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,17 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
+import {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+} from "../controllers/userController.js";
 
+router.post('/', registerUser);
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
-router.post('/auth', authUser)
 
 export default router;
