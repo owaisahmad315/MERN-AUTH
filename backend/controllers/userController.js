cc2471e9dfa15f1c89e09f455bb803e30685ce7a
Owais Ahmad <owaisahmad014@gmail.com>
2023-07-11 00:03:58 +0500

    .

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,8 +18,31 @@ const authUser = asyncHandler(async (req, res) => {
 
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
-    console.log(name);
-    res.status(200).json({ message: 'Register user' });
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already Exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data')
+    }
+
 });
 
 
