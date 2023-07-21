5860ddb89e14e1b62c6920edc67e713fae17601c
Owais Ahmad <owaisahmad014@gmail.com>
2023-07-21 20:11:41 +0500

    navbar is completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
- import React from 'react'
+ import React from 'react';
+ import Header from "./components/Header";
  
  const App = () => {
    return (
       <>
+      <Header />
         <h1>Mern Auth</h1>
       </>
    )
