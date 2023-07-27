e77c966926735baa6d96463c8f3b90d76eb59685
Owais Ahmad <owaisahmad014@gmail.com>
2023-07-27 19:02:10 +0500

    created slices

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice'
 
 
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: authReducer,
+    },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
     devTools: true,
 });
