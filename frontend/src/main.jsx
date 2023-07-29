77b378960da64c02db4be356186ec34b34c43797
Owais Ahmad <owaisahmad014@gmail.com>
2023-07-29 20:28:35 +0500

    implemented private route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,9 +12,11 @@ import { Provider } from "react-redux";
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import PrivateRoute from "./components/PrivateRoute";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ProfileScreen from "./screens/ProfileScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,6 +24,10 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />}/>
       <Route path="/login" element={<LoginScreen />}/>
       <Route path="/register" element={<RegisterScreen />}/>
+      {/* PrivateRoute is a component */}
+      <Route path="" element={<PrivateRoute />}>                    
+        <Route path="/profile" element={<ProfileScreen />}/>
+      </Route>
     </Route>
   )
 )
